d86dd490bc7fec7443fff34a44a807a17be57efe
Maho Okudaira <okudaira.m.196@ms.saitama-u.ac.jp>
2026-07-22 00:26:56 +0900

    一旦完成ver.1.0

--- interact_list1.cpp
+++ interact_list1.cpp
@@ -22,6 +22,16 @@ const int MAX_RECIPES = 16;
 // レシピの個数
 int recipeCount = 0;
 
+// SEED期間
+int SeedTime = 3;
+// GROWING期間
+int GrowingTime = 10;
+
+// 種のインベントリ
+int seedCount[CROP_COUNT];
+// 作物のインベントリ
+int cropCount[CROP_COUNT];
+
 // 土の状態
 enum CropStage {
 	EMPTY,      // 空
@@ -41,6 +51,7 @@ enum CropType {
 struct Cell {
 	CropStage stage;
 	int cropType;
+	int growthTime;
 };
 
 // 交配結果のフォーマット
@@ -59,8 +70,8 @@ struct CrossbreedRecipe {
 
 CrossbreedRecipe recipes[MAX_RECIPES];
 
+// レシピ設定
 void initRecipes(void) {
-	// レシピ0: 小麦 × 小麦 → 小麦70%, ニンジン30%
 	recipes[0].typeA = CROP_WHEAT;
 	recipes[0].typeB = CROP_WHEAT;
 	recipes[0].results[0].cropType = CROP_WHEAT;
@@ -114,6 +125,15 @@ void initRecipes(void) {
 	recipeCount += 1;
 }
 
+// インベントリの初期化
+void initInventory(void) {
+	for (int i = 0; i < CROP_COUNT; i++) {
+		seedCount[i] = 0;
+		cropCount[i] = 0;
+	}
+	seedCount[CROP_WHEAT] = 2;
+}
+
 // 土のマス目
 Cell grid[MAX_SIZE][MAX_SIZE];
 void initGrid(void) {
@@ -121,6 +141,7 @@ void initGrid(void) {
 		for (int j = 0; j < MAX_SIZE; j++) {
 			grid[i][j].stage = EMPTY;
 			grid[i][j].cropType = CROP_NONE;
+			grid[i][j].growthTime = 0;
 		}
 	}
 }
@@ -155,11 +176,11 @@ int isMature(int x, int z) {
 // 2つの作物の組み合わせに合うレシピの番号を返す。（なければ-1を返す）
 int findRecipe(int typeA, int typeB) {
 	for (int r = 0; r < recipeCount; r++) {
-		// 順方向でマッチ
+		// 増加方向でマッチ
 		if (recipes[r].typeA == typeA && recipes[r].typeB == typeB) {
 			return r;
 		}
-		// 逆方向でマッチ
+		// 減少方向でマッチ
 		if (recipes[r].typeA == typeB && recipes[r].typeB == typeA) {
 			return r;
 		}
@@ -250,6 +271,7 @@ void checkCrossbreed(int x, int z)
 			if (randValue < cumulativeWeight) {
 				grid[x][z].stage = SEED;
 				grid[x][z].cropType = i;
+				grid[x][z].growthTime = 0;
 				break;
 			}
 		}
@@ -271,10 +293,22 @@ void timer(int value)
 		for (int j = 0; j < gridSize_z; j++) {
 			// 作物の成長
 			if (grid[i][j].stage == SEED) {
-				grid[i][j].stage = GROWING;
+				if (grid[i][j].growthTime < SeedTime) {
+					grid[i][j].growthTime += 1;
+				}
+				else {
+					grid[i][j].stage = GROWING;
+					grid[i][j].growthTime = 0;
+				}
 			}
 			else if (grid[i][j].stage == GROWING) {
-				grid[i][j].stage = MATURE;
+				if (grid[i][j].growthTime < GrowingTime) {
+					grid[i][j].growthTime += 1;
+				}
+				else {
+					grid[i][j].stage = MATURE;
+					grid[i][j].growthTime = 0;
+				}
 			}
 		}
 	}
@@ -283,29 +317,42 @@ void timer(int value)
 	glutTimerFunc(1000, timer, 0);
 }
 
+// 選択中の種
+int selectedSeed = CROP_WHEAT;
+
 void keyboard(unsigned char key, int x, int y)
 {
 	switch (key) {
 		case 'p':
 			printf("(%3d,%3d)でpが押されました\n", x, y);
-			if (grid[select_x][select_z].stage == EMPTY) {
+			if (grid[select_x][select_z].stage == EMPTY && seedCount[selectedSeed] > 0) {
+				seedCount[selectedSeed] -= 1;
 				grid[select_x][select_z].stage = SEED;
-				grid[select_x][select_z].cropType = CROP_WHEAT;
+				grid[select_x][select_z].cropType = selectedSeed;
+				grid[select_x][select_z].growthTime = 0;
 			}
 			break;
 		case 'h':
 			if (grid[select_x][select_z].stage == MATURE) {
+				int type = grid[select_x][select_z].cropType;
+				seedCount[type] += 2;
+				cropCount[type] += 1;
 				grid[select_x][select_z].stage = EMPTY;
 				grid[select_x][select_z].cropType = CROP_NONE;
+				grid[select_x][select_z].growthTime = 0;
+				printf("収穫: type=%d 種=%d 作物=%d\n", type, seedCount[type], cropCount[type]);
 			}
 			break;
 		case '1':
+			selectedSeed = CROP_WHEAT;
 			printf("(%3d,%3d)で1が押されました\n", x, y);
 			break;
 		case '2':
+			selectedSeed = CROP_CARROT;
 			printf("(%3d,%3d)で2が押されました\n", x, y);
 			break;
 		case '3':
+			selectedSeed = CROP_TOMATO;
 			printf("(%3d,%3d)で3が押されました\n", x, y);
 			break;
 	}
@@ -415,57 +462,43 @@ void reshape(int w, int h)
 	glLoadIdentity();
 	gluLookAt(5.0, 5.0, 5.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0); // 視点の設定
 }
+// 文字列を描画する関数
+void render_string(float x, float y, float z, const char* str) {
+	glDisable(GL_LIGHTING);
+	glColor3f(0.0, 0.0, 0.0);
+	glRasterPos3f(x, y, z);
+	const char* c = str;
+	while (*c) {
+		glutBitmapCharacter(GLUT_BITMAP_HELVETICA_18, *c++);
+	}
+	glEnable(GL_LIGHTING);
+}
 
 void display(void){            // 描画時に呼び出される関数（Displayコールバック関数)
-	glClearColor(0.0, 0.0, 0.0, 1.0); // 画面クリア
+	glClearColor(0.9, 0.9, 1.0, 1.0); // 画面クリア
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // 画面バッファクリア
 	glEnable(GL_DEPTH_TEST); // 隠面消去を有効
 	// 畑を中心に寄せるための変数設定
 	float MovingCube_x = ((float)gridSize_x - 1.0) / 2.0;
 	float MovingCube_z = ((float)gridSize_z - 1.0) / 2.0;
+	
 	for (int i = 0; i < gridSize_x; i++) {
 		for (int j = 0; j < gridSize_z; j++) {
-			// 材質
-			switch (grid[i][j].stage) {
-				case EMPTY: {
-					GLfloat mat0ambi[] = { 0.25,  0.17, 0.10, 1.0 }; // 土
-					glMaterialfv(GL_FRONT_AND_BACK, GL_AMBIENT, mat0ambi); //環境光の反射率を設定
-					GLfloat mat0diff[] = { 0.50,  0.35, 0.20, 1.0 }; // 土
-					glMaterialfv(GL_FRONT_AND_BACK, GL_DIFFUSE, mat0diff); //拡散光の反射率を設定
-					break;
-				}
-				case SEED: {
-					GLfloat mat0ambi[] = { 0.27,  0.22, 0.10, 1.0 }; // 土
-					glMaterialfv(GL_FRONT_AND_BACK, GL_AMBIENT, mat0ambi); //環境光の反射率を設定
-					GLfloat mat0diff[] = { 0.55,  0.45, 0.20, 1.0 }; // 土
-					glMaterialfv(GL_FRONT_AND_BACK, GL_DIFFUSE, mat0diff); //拡散光の反射率を設定
-					break;
-				}
-				case GROWING: {
-					GLfloat mat0ambi[] = { 0.32,  0.27, 0.15, 1.0 }; // 土
-					glMaterialfv(GL_FRONT_AND_BACK, GL_AMBIENT, mat0ambi); //環境光の反射率を設定
-					GLfloat mat0diff[] = { 0.65,  0.55, 0.30, 1.0 }; // 土
-					glMaterialfv(GL_FRONT_AND_BACK, GL_DIFFUSE, mat0diff); //拡散光の反射率を設定
-					break;
-				}
-				case MATURE: {
-					GLfloat mat0ambi[] = { 0.37,  0.32, 0.25, 1.0 }; // 土
-					glMaterialfv(GL_FRONT_AND_BACK, GL_AMBIENT, mat0ambi); //環境光の反射率を設定
-					GLfloat mat0diff[] = { 0.75,  0.65, 0.50, 1.0 }; // 土
-					glMaterialfv(GL_FRONT_AND_BACK, GL_DIFFUSE, mat0diff); //拡散光の反射率を設定
-					break;
-				}
-			}
-			GLfloat mat0spec[] = { 0.0,  0.0, 0.0, 1.0 };    // 土
-			glMaterialfv(GL_FRONT_AND_BACK, GL_SPECULAR, mat0spec); //鏡面光の反射率を設定
-			GLfloat mat0shine[] = { 0.00 };                  // 土
-			glMaterialfv(GL_FRONT_AND_BACK, GL_SHININESS, mat0shine);
 			glPushMatrix();
+				// 材質
+				GLfloat mat0ambi[] = { 0.25,  0.17, 0.10, 1.0 }; // 土
+				glMaterialfv(GL_FRONT_AND_BACK, GL_AMBIENT, mat0ambi); //環境光の反射率を設定
+				GLfloat mat0diff[] = { 0.50,  0.35, 0.20, 1.0 }; // 土
+				glMaterialfv(GL_FRONT_AND_BACK, GL_DIFFUSE, mat0diff); //拡散光の反射率を設定
+				GLfloat mat0spec[] = { 0.0,  0.0, 0.0, 1.0 };    // 土
+				glMaterialfv(GL_FRONT_AND_BACK, GL_SPECULAR, mat0spec); //鏡面光の反射率を設定
+				GLfloat mat0shine[] = { 0.00 };                  // 土
+				glMaterialfv(GL_FRONT_AND_BACK, GL_SHININESS, mat0shine);
 				// 場所
 				glTranslatef((float)i - MovingCube_x, 0.0, (float)j - MovingCube_z);
 				// 物体の描画
 				glutSolidCube(1.0);	// 立方体
-				// 選択中のマスをデフォルメ
+				// 選択中のマスのまわりにwireを描画する
 				if (i == select_x && j == select_z) {
 					GLfloat frameDiff[] = { 1.0, 1.0, 1.0, 1.0 };
 					glMaterialfv(GL_FRONT_AND_BACK, GL_DIFFUSE, frameDiff);
@@ -474,9 +507,76 @@ void display(void){            // 描画時に呼び出される関数（Display
 
 					glutWireCube(1.01);    // 土より少し大きい線の箱
 				}
+				if (grid[i][j].stage != EMPTY) {
+					glPushMatrix();
+						switch (grid[i][j].cropType) {
+							case CROP_WHEAT: {
+								GLfloat d[] = { 0.85, 0.75, 0.25, 1.0 };   // 黄色
+								glMaterialfv(GL_FRONT_AND_BACK, GL_DIFFUSE, d);
+								GLfloat a[] = { 0.42, 0.37, 0.12, 1.0 };
+								glMaterialfv(GL_FRONT_AND_BACK, GL_AMBIENT, a);
+								break;
+							}
+							case CROP_CARROT: {
+								GLfloat d[] = { 0.90, 0.50, 0.15, 1.0 };   // オレンジ
+								glMaterialfv(GL_FRONT_AND_BACK, GL_DIFFUSE, d);
+								GLfloat a[] = { 0.45, 0.25, 0.07, 1.0 };
+								glMaterialfv(GL_FRONT_AND_BACK, GL_AMBIENT, a);
+								break;
+							}
+							case CROP_TOMATO: {
+								GLfloat d[] = { 0.80, 0.20, 0.20, 1.0 };   // 赤
+								glMaterialfv(GL_FRONT_AND_BACK, GL_DIFFUSE, d);
+								GLfloat a[] = { 0.40, 0.10, 0.10, 1.0 };
+								glMaterialfv(GL_FRONT_AND_BACK, GL_AMBIENT, a);
+								break;
+							}
+						}
+						switch (grid[i][j].stage) {
+							case SEED: {
+								glTranslatef(0.0, 0.5, 0.0);
+								glScalef(0.2, 0.2, 0.2);
+								glutSolidSphere(1.0, 20, 20);
+								break;
+							}
+							case GROWING: {
+								glTranslatef(0.0, 0.7, 0.0);
+								glScalef(0.1, 0.5, 0.1);
+								glutSolidCube(1.0);
+								break;
+							}
+							case MATURE: {
+								glTranslatef(0.0, 0.7, 0.0);
+								glScalef(0.5, 0.5, 0.5);
+								glutSolidSphere(1.0, 20, 20);
+								break;
+							}
+						}
+					glPopMatrix();
+				}
 			glPopMatrix();
 		}
 	}
+	char tmp[256];
+	sprintf_s(tmp, sizeof(tmp), "Wheat seed:%d  crop:%d", seedCount[CROP_WHEAT], cropCount[CROP_WHEAT]);
+	render_string(2.90, 4.15, 4.0, tmp);
+	sprintf_s(tmp, sizeof(tmp), "Carrot seed:%d  crop:%d", seedCount[CROP_CARROT], cropCount[CROP_CARROT]);
+	render_string(2.88, 4.10, 4.0, tmp);
+	sprintf_s(tmp, sizeof(tmp), "Tomato seed:%d  crop:%d", seedCount[CROP_TOMATO], cropCount[CROP_TOMATO]);
+	render_string(2.86, 4.05, 4.0, tmp);
+	sprintf_s(tmp, sizeof(tmp), "Selected crop: %d", selectedSeed);
+	render_string(2.84, 4.00, 4.0, tmp);
+	sprintf_s(tmp, sizeof(tmp), "key to select Wheat: 1");
+	render_string(0.00, 2.25, 0.0, tmp);
+	sprintf_s(tmp, sizeof(tmp), "key to select Carrot: 2");
+	render_string(0.00, 2.10, 0.0, tmp);
+	sprintf_s(tmp, sizeof(tmp), "key to select Tomato: 3");
+	render_string(0.00, 1.95, 0.0, tmp);
+	sprintf_s(tmp, sizeof(tmp), "key to plant: p");
+	render_string(0.00, 1.80, 0.0, tmp);
+	sprintf_s(tmp, sizeof(tmp), "key to harvest: h");
+	render_string(0.00, 1.65, 0.0, tmp);
+
 	glutSwapBuffers(); // 描画実行
 }
 
@@ -517,6 +617,7 @@ int main(int argc, char *argv[])
 	glutSpecialUpFunc(specialKeyUp);// 特殊キーコールバック関数の指定(離したとき)
 	initGrid();     // 土キューブの初期化
 	initRecipes();  // レシピの設定
+	initInventory(); 			    // インベントリの初期化
 	lightInit();    // 光源の初期設定(まとめて関数にしているだけ)
 	glutTimerFunc(1000, timer, 0);
 	glutMainLoop(); // メインループへ
